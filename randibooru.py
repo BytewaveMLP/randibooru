c61311dae4401c07089436e0515f59fb67a13afa
Eliot Partridge <shortcircuit369@gmail.com>
2017-01-28 22:27:51 -0600

    Lotsa changes:
    - Reply to **ONLY** !rb
    - Remove user blacklist feature
    - Truncate after the first 20 tags on the image, if available
    - Give the score, upvotes, downvotes, and favorites in the response embed
    - Randomly color the sidebar on every embed response

--- randibooru.py
+++ randibooru.py
@@ -7,6 +7,7 @@ USER_BLACKLIST = []
 
 import discord
 import asyncio
+import random
 from derpibooru import Search, sort
 
 client = discord.Client()
@@ -22,35 +23,42 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-	if message.content.startswith(COMMAND_PREFIX + COMMAND_NAME):
+	if message.content == "!rb" or message.content.startswith(COMMAND_PREFIX + COMMAND_NAME + " "):
 		print('Request received!') # Debug and logging
 		requester = message.author
 		print('Requester:', requester.name, '|', requester.id)
 
-		if requester.id not in USER_BLACKLIST:
-			query = message.content[(len(COMMAND_PREFIX + COMMAND_NAME) + 1):].strip() # Strip command from message text
-			print('Query:    ', query)
-			client.send_typing(message.channel)
+		query = message.content[(len(COMMAND_PREFIX + COMMAND_NAME) + 1):].strip() # Strip command from message text
+		print('Query:    ', query)
+		client.send_typing(message.channel)
 
-			search  = Search().query(query).key(DERPIBOORU_API_TOKEN).sort_by(sort.RANDOM).limit(1) # DerPyBooru searching
-			results = list(search)
+		search  = Search().query(query).key(DERPIBOORU_API_TOKEN).sort_by(sort.RANDOM).limit(1) # DerPyBooru searching
+		results = list(search)
 
-			responseStr = requester.mention + (' (query: `' + query + '`)' if query != '' else '')
+		responseStr = requester.mention + (' (query: `' + query + '`)' if query != '' else '')
 
-			if len(results) == 0:
-				print('Result:    No images found.')
-				await client.send_message(message.channel, responseStr + ' No images found.')
-			else:
-				result = results[0]
-				print('Result:   ', result.url)
-
-				em = discord.Embed(title = "Derpibooru Image", description = ", ".join(result.tags), url = result.url)
-				em.set_image(url = result.image)
-				em.set_footer(text = "Randibooru - Made with <3 by Bytewave")
-
-				await client.send_message(message.channel, responseStr, embed = em)
+		if len(results) == 0:
+			print('Result:    No images found.')
+			await client.send_message(message.channel, responseStr + ' No images found.')
 		else:
-			print("User does not have access.")
+			result = results[0]
+			print('Result:   ', result.url)
+
+			if len(result.tags) > 20:
+				tags = ", ".join(result.tags[:20]) + "..."
+			else:
+				tags = ", ".join(result.tags)
+
+			color = random.randint(0, 16777215)
+
+			em = discord.Embed(title = "Derpibooru Image", url = result.url, color = color)
+			em.set_image(url = result.image)
+			em.add_field(name = "Tags", value = tags, inline = False)
+			em.add_field(name = "Score", value = "{score} (+{upvotes}/-{downvotes})".format(score = result.score, upvotes = result.upvotes, downvotes = result.downvotes), inline = True)
+			em.add_field(name = "Favorites", value = result.faves, inline = True)
+			em.set_footer(text = "Randibooru - Made with <3 by Bytewave")
+
+			await client.send_message(message.channel, responseStr, embed = em)
 
 		print('========')
 

2211d4c78c2137e7b352ae0151df7dc8eacbe35c
Eliot Partridge <shortcircuit369@gmail.com>
2017-03-10 19:33:00 -0600

    Add a configuration option (FINALLY)

--- randibooru.py
+++ randibooru.py
@@ -1,12 +1,32 @@
-DISCORD_API_TOKEN    = ""
-DERPIBOORU_API_TOKEN = ""
-COMMAND_PREFIX       = "!"
-COMMAND_NAME         = "rb"
-
 import discord
 import asyncio
 import random
 from derpibooru import Search, sort
+import configparser
+import os.path
+import sys
+
+config = configparser.ConfigParser()
+
+if os.path.isfile('./config.ini'):
+	config.read('./config.ini')
+else:
+	config['Command'] = {'Prefix': '!',
+	                     'Name': 'rb'}
+	config['API keys'] = {'Derpibooru': '',
+	                      'Discord': ''}
+
+	with open('./config.ini', 'w') as cfgfile:
+		config.write(cfgfile)
+
+	print('Config file generated!')
+	print('Edit config.ini before starting Randibooru.')
+	sys.exit(1)
+
+DISCORD_API_TOKEN    = config.get('API keys', 'Discord')
+DERPIBOORU_API_TOKEN = config.get('API keys', 'Derpibooru')
+COMMAND_PREFIX       = config.get('Command', 'Prefix', fallback='!')
+COMMAND_NAME         = config.get('Command', 'Name', fallback='rb')
 
 client = discord.Client()
 helpgame = discord.Game(name = COMMAND_PREFIX + COMMAND_NAME + " <derpi query>", url = "", type = 0)
@@ -50,7 +70,7 @@ async def on_message(message):
 			color = random.randint(0, 16777215)
 
 			em = discord.Embed(title = "Derpibooru Image", url = result.url, color = color)
-			em.set_image(url = result.image)
+			em.set_image(url = result.)
 			em.add_field(name = "Tags", value = tags, inline = False)
 			em.add_field(name = "Score", value = "{score} (+{upvotes}/-{downvotes})".format(score = result.score, upvotes = result.upvotes, downvotes = result.downvotes), inline = True)
 			em.add_field(name = "Favorites", value = result.faves, inline = True)

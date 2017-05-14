f1ef5e75965b2d2169b19476a788e81e301eb36d
Eliot Partridge <shortcircuit369@gmail.com>
2017-05-14 16:57:20 -0500

    The logging update!
    - Added a whole bunch of logging stuff (better than print!)
    - PUll 50 (by default) images per request, not just one (yay for DerPyBooru patches)
    - Bugfix in explicit image filter
    - Formatting tweaks in response messages
    - Some variable renames, etc
    
    NOTE: You will need to add the following lines to your configs:
    [Logging]
    level = INFO
    
    [Other]
    imagesperrequest = 50

--- randibooru.py
+++ randibooru.py
@@ -6,6 +6,13 @@ import configparser
 import os.path
 import sys
 import traceback
+import logging
+
+log = logging.getLogger('randibooru')
+formatter = logging.Formatter('[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s')
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setFormatter(formatter)
+log.addHandler(console_handler)
 
 config = configparser.ConfigParser()
 
@@ -16,13 +23,22 @@ else:
 	                     'Name': 'rb'}
 	config['API keys'] = {'Derpibooru': '',
 	                      'Discord': ''}
+	config['Logging'] = {'Level': 'INFO'}
+	config['Other'] = {'ImagesPerRequest': 50}
 
 	with open('./config.ini', 'w') as cfgfile:
 		config.write(cfgfile)
 
-	print('Config file generated!')
-	print('Edit config.ini before starting Randibooru.')
+	log.info('Config file generated!')
+	log.info('Edit config.ini before starting Randibooru.')
 	sys.exit(1)
+
+level = logging.getLevelName(config.get('Logging', 'Level'))
+
+try:
+	log.setLevel(level = level)
+except ValueError:
+	log.setLevel(level = logging.INFO)
 
 DISCORD_API_TOKEN    = config.get('API keys', 'Discord')
 DERPIBOORU_API_TOKEN = config.get('API keys', 'Derpibooru')
@@ -35,26 +51,20 @@ helpgame = discord.Game(name = COMMAND_PREFIX + COMMAND_NAME + " <derpi query>",
 
 @client.event
 async def on_ready():
-	print('Logged in as:')
-	print(client.user.name)
-	print(client.user.id)
-	print('------')
+	log.info('Logged in as ' + client.user.name + ' (' + client.user.id + ')')
+	log.info('Use this link to invite me to your server: ' + discord.utils.oauth_url(client.user.id, permissions = discord.Permissions(permissions = 3072)))
 	await client.change_presence(game = helpgame, afk = False)
 
 @client.event
 async def on_error(event, *args, **kwargs):
-	ex = sys.exc_info()
-	print('!!!!!!')
-	print('ERROR:     ' + ex[0].__name__ + ' occured in ' + event + '!')
-	print('           ' + str(ex[1]))
-	print('Traceback:')
-	print("".join(traceback.format_tb(ex[2])), end = '')
-	print('!!!!!!')
+	log.error('Exception occurred in ' + event, exc_info = True)
 
 @client.event
 async def on_server_join(server):
 	general = server.default_channel
+	log.info('Joined server ' + server.name + ' (' + server.id + ')')
 	if server.me.permissions_in(general).send_messages:
+		log.debug('Able to post messages in general channel. Sending welcome message...')
 		await client.send_message(general, "**Hey there!** I'm Randibooru! I pull random images from Derpibooru with an optional Derpibooru query.\n"
 			+ "To get a random image, simply type `" + COMMAND + "` in chat, and I'll try to respond. You can optionally follow `" + COMMAND + "` with a Derpibooru query (info here: https://derpibooru.org/search/syntax), and I'll only pull images that match it.\n"
 			+ "Have fun!\n\n"
@@ -65,45 +75,45 @@ async def on_server_join(server):
 @client.event
 async def on_message(message):
 	if (message.content == COMMAND) or message.content.startswith(COMMAND + " "):
-		print('Request received!') # Debug and logging
 		requester = message.author
-		print('Requester:', requester.name, '|', requester.id)
-
 		query = message.content[(len(COMMAND) + 1):].strip() # Strip command from message text
-		print('Query:    ', query)
+		log_user_str = 'from ' + requester.name + ' (' + requester.id + ')' + (' with query ' + query if query != '' else '')
+		log.info('Request received ' + log_user_str)
 		await client.send_typing(message.channel)
 
-		responseStr = requester.mention + (' (query: `' + query + '`)' if query != '' else '')
+		response_str = requester.mention + (' (query: `' + query + '`)' if query != '' else '')
 
-		search  = Search().query(query).key(DERPIBOORU_API_TOKEN).sort_by(sort.RANDOM).limit(1) # DerPyBooru searching
+		search  = Search().query(query).key(DERPIBOORU_API_TOKEN).sort_by(sort.RANDOM).limit(int(config.get('Other', 'ImagesPerRequest') or 50)) # DerPyBooru searching
 
 		results = list(search)
 
 		if len(results) == 0:
-			print('Result:    No images found.')
-			await client.send_message(message.channel, responseStr + ' No images found.')
+			log.info('No results found for request ' + log_user_str)
+			await client.send_message(message.channel, response_str + ' - *No images found.*')
 		else:
 			result = None
 
-			print('Channel:  ', message.channel.name)
+			log.debug('Request ' + log_user_str + ' in channel ' + message.channel.name + ' (' + message.channel.id + ')')
 			if message.channel.name != 'nsfw' and not message.channel.name.startswith('nsfw-'):
-				print('Type:      Safe channel')
+				log.debug('Request ' + log_user_str + ' was sent in a SAFE channel')
 				for potential in results: # Leaving this in for future modification - need to patch DerPyBooru before this becomes relevant
 					if 'explicit' not in potential.tags:
 						result = potential
+						break
+					log.debug('Skipping unsuitable image ' + potential.url + ' for request ' + log_user_str)
 			else:
-				print('Type:      NSFW channel')
+				log.debug('Request ' + log_user_str + ' was sent in an NSFW channel')
 				result = results[0]
 
 			if result is None:
-				print('Result:    Failed to find any safe content (non-NSFW channel)')
-				await client.send_message(message.channel, responseStr + " The image I found wasn't safe! Try again, or call me in an NSFW channel for `explicit` images!")
-				print('========')
+				log.info('Couldn\'t find any safe images to post for request ' + log_user_str)
+				await client.send_message(message.channel, response_str + " - *I couldn't find any safe images! Try again, or call me in an NSFW channel for `explicit` images!*")
 				return
 			
-			print('Result:   ', result.url)
+			log.info('Found suitable result ' + result.url + ' for request ' + log_user_str)
 
 			if len(result.tags) > 20:
+				log.debug('Limiting displayed tags to 20 for request ' + log_user_str + ' to keep Discord from yelling at us')
 				tags = ", ".join(result.tags[:20]) + "..."
 			else:
 				tags = ", ".join(result.tags)
@@ -117,8 +127,12 @@ async def on_message(message):
 			em.add_field(name = "Favorites", value = result.faves, inline = True)
 			em.set_footer(text = "Randibooru - Made with <3 by Bytewave")
 
-			await client.send_message(message.channel, responseStr, embed = em)
+			log.debug('Sending embed for request ' + log_user_str)
+			await client.send_message(message.channel, response_str, embed = em)
 
-		print('========')
+log.info('Randibooru bot starting...')
 
-client.run(DISCORD_API_TOKEN)
+try:
+	client.run(DISCORD_API_TOKEN)
+except:
+	log.critical('An error occurred while starting Randibooru', exc_info = True)
